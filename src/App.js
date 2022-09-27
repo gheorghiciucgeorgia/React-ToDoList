7f827932341aa1d18ed0b78767cb6dcba3a335f4
gheorghiciucgeorgia <georgia-elena.gheorghiciuc@studen.tuiasi.ro>
2022-09-27 13:38:51 +0300

    functionality compelte

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
-import React, {useState} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   //useState - is a react hook
-  //
-  useState([])
+
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  //storage the added todo in localstorage 
+  //when you refresh the page the todoList stays unchanged
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(previousTodos => [...previousTodos, ...storedTodos]);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]);
+
+  //changing the state of the checknox using this function
+  function toggleTodo(id) {
+
+    //created a copy of the todo list
+    //never modify the direct objects
+    const newTodos = [...todos]
+
+    //getting the todos we actually want to modify
+    //finding the todo that has the same id
+    const todo = newTodos.find(todo => todo.id === id)
+    
+    //and when we click the toggle we will change the state of the complete
+    todo.complete = !todo.complete
+    //setting the todos to the newTodos that had been changed
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(previousTodos => {
+      return [...previousTodos, { id: uuidv4(), name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
+  }
+
+  //clearing the Done Todos
+  function handleClearTodo(){
+    const newTodos=todos.filter(todos => !todos.complete)
+    setTodos(newTodos)
+  }
+
   return (
     // This "<>" and </> is called a fragment and can
     // contain more html elements
     <>
-      <TodoList />
-      <input type="text" />
-      <button>Add ToDo</button>
-      <button>Clear Done ones</button>
-      <div>0 left to do</div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add ToDo</button>
+      <button onClick={handleClearTodo}>Clear done ones</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
 
   );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import TodoComponent from './TodoComponent'
 
-export default function TodoList() {
+export default function TodoList({ todos, toggleTodo }) {
   return (
-    <div>
-      Hello World form todo file
-    </div>
+    todos.map(todo => {
+      return <TodoComponent key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+    })
   )
 }
